aff64b6684f491f69ca2bde268461b5737d2cd9f
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-06-13 07:45:30 -0700

    Update the script file of the js

--- HealthAndBeauty/assets/Bootstrap4/js/script.js
+++ HealthAndBeauty/assets/Bootstrap4/js/script.js
@@ -77,41 +77,41 @@ $(document).ready(function () {
 $(function () {
     $('#owl-one').owlCarousel({
         loop: true,
-        margin: 20,
+        margin: 24,
         dots: false,
         nav: true,
-        slideBy: 1,
-        item: 1,
+        slideBy: 4,
+        item: 4,
         navText: ["<i class='flaticon-last-track-left-arrow'> </i>", "<i class='flaticon-right-arrow'> </i>"],
         responsiveClass: true,
         responsive: {
             0: {
                 items: 1,
-                dots: true,
-                nav: false
+                dots: false,
+                nav: true,
             },
             400: {
                 items: 1,
-                dots: true,
-                nav: false
+                dots: false,
+                nav: true,
             },
             600: {
-                items: 1,
+                items: 2,
                 dots: false,
                 nav: true
             },
             768: {
-                items: 1,
+                items: 3,
                 dots: false,
                 nav: true
             },
             830: {
-                items: 1,
+                items: 3,
                 dots: false,
                 nav: true
             },
             1000: {
-                items: 1,
+                items: 4,
                 dots: false,
                 nav: true
             }
@@ -183,10 +183,10 @@ $(function () {
 
 /*===================== Another Load More Script ======================*/
 $(function () {
-    $(".no-display").slice(0, 9).show();
+    $(".no-display").slice(0, 8).show();
     $("#load-more").on('click', function (e) {
         e.preventDefault();
-        $(".no-display:hidden").slice(0, 3).slideDown();
+        $(".no-display:hidden").slice(0, 4).slideDown();
         if ($(".no-display:hidden").length == 0) {
             $("#load-more").fadeOut('slow');
 
@@ -198,3 +198,71 @@ $(function () {
     });
 });
 /*===================== Filter Section Script ======================*/
+jQuery(document).ready(function () {
+
+    function detect_active() {
+        // get active
+        var get_active = $("#dp-slider .dp_item:first-child").data("class");
+        $("#dp-dots li").removeClass("active");
+        $("#dp-dots li[data-class=" + get_active + "]").addClass("active");
+    }
+    $("#dp-next").click(function () {
+        var total = $(".dp_item").length;
+        $("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn();
+        $.each($('.dp_item'), function (index, dp_item) {
+            $(dp_item).attr('data-position', index + 1);
+        });
+        detect_active();
+
+    });
+
+    $("#dp-prev").click(function () {
+        var total = $(".dp_item").length;
+        $("#dp-slider .dp_item:last-child").hide().prependTo("#dp-slider").fadeIn();
+        $.each($('.dp_item'), function (index, dp_item) {
+            $(dp_item).attr('data-position', index + 1);
+        });
+
+        detect_active();
+    });
+
+    $("#dp-dots li").click(function () {
+        $("#dp-dots li").removeClass("active");
+        $(this).addClass("active");
+        var get_slide = $(this).attr('data-class');
+        console.log(get_slide);
+        $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
+        $.each($('.dp_item'), function (index, dp_item) {
+            $(dp_item).attr('data-position', index + 1);
+        });
+    });
+
+
+    $("body").on("click", "#dp-slider .dp_item:not(:first-child)", function () {
+        var get_slide = $(this).attr('data-class');
+        console.log(get_slide);
+        $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
+        $.each($('.dp_item'), function (index, dp_item) {
+            $(dp_item).attr('data-position', index + 1);
+        });
+
+        detect_active();
+    });
+});
+/*===================== Filter Section Script ======================*/
+$(function () {
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 100) {
+            $("nav").addClass("nav-bg-color");
+
+        } else {
+            $("nav").removeClass("nav-bg-color");
+
+
+        }
+
+
+    });
+});
